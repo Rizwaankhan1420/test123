a83c00540f0a1a7276b651f172c0c165a1586f86
Rizwan Khan <Rizwaankhan1420@gmail.com>
2024-04-30 14:59:21 +0000

    asdded same function to main branch

--- file_add.c
+++ file_add.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
-
 int main() {
-
 int num1, num2, sum;
-
 // Input the two numbers
-
 printf("Enter first number: ");
-
 scanf("%d", &num1);
-
 printf("Enter second number: ");
-
 scanf("%d", &num2);
-
 // Add the numbers
-
 sum num1 + num2;
-
 // Display the result
-
 printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+return 0;
+}
+int add fucn(int a, int b)
+{ int  c=a+b;
+return c;
+}
+//added in branch1
+>>>>>branch1
+
+#include <stdio.h>
+Int main() { int num1, num2, sum;
+// Input the two numbers
+printf("Enter first number: ");
+scanf("%d", &numi);
+printf("Enter second number: ");
+scanf("%d" &num2);
+// Add the numbers
+sum = num1 + num2;
+// Display the result
+printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+return 0;
+
+int addfunc(int a, int b)
+}
+}
+int ca+b;
+return c;
+//conflict resolved
+
 
